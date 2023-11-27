2005ea84f9bdfd752dd847a5be5491e30ce5fb54
Athiwat Wiroonphattanadate <athiwat.wir@gmail.com>
2023-11-27 17:35:13 +0700

    active menu

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -29,67 +29,67 @@
   <nav class="nav-deep nav-deep-sm nav-deep-light">
     <ul class="nav flex-column">
 
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('booking*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('booking-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Booking</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('account*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('users-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Administation</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('stations*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('stations-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Station Manage</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('route-control*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('route-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Route Manage</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('promotion*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('promotion-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Promotion</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('time-table*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('time-table-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Time Table</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('route-map*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('route-map-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Route Map</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('stations-info*')) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('stations-info-index') }}">
           <i class="fi fi-arrow-right"></i>
           <span>Station Infomation</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('news*')) ? 'active' : '' }}">
         <a class="nav-link" href="#">
           <i class="fi fi-arrow-right"></i>
           <span>News</span>
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('meals*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('meals-index') }}">
               <i class="fi fi-arrow-right"></i>
               <span>Meal on board</span>
           </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('activity*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('activity-index') }}">
               <i class="fi fi-arrow-right"></i>
               <span>Activity</span>
@@ -139,13 +139,13 @@
               <span>Report</span>
           </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('fare-manage*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('fare-index') }}">
               <i class="fi fi-arrow-right"></i>
               <span>Fare Manage</span>
           </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->is('information*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('information-index') }}">
               <i class="fi fi-arrow-right"></i>
               <span>Infomation</span>

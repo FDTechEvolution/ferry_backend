95f3ed5dc04dcf91c7e65fead20a988d8a94daf0
New <aio4t9@protonmail.com>
2025-09-29 23:17:46 +0700

    update

--- app/Helpers/RouteHelper.php
+++ app/Helpers/RouteHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Helpers;
 
 use App\Models\Route;
@@ -20,9 +21,9 @@ class RouteHelper
         $_departDate = Carbon::createFromFormat('Y-m-d', $departDate);
         $routeIds = [];
 
-        $routeDailyStatuses = RouteDailyStatus::select('route_id')->where('date',$departDate)->where('isopen','Y')->get();
-        foreach($routeDailyStatuses as $routeDailyStatus){
-            array_push($routeIds,$routeDailyStatus->route_id);
+        $routeDailyStatuses = RouteDailyStatus::select('route_id')->where('date', $departDate)->where('isopen', 'Y')->get();
+        foreach ($routeDailyStatuses as $routeDailyStatus) {
+            array_push($routeIds, $routeDailyStatus->route_id);
         }
 
         //dd($routeDailyStatuses);
@@ -30,9 +31,9 @@ class RouteHelper
         $routes = Route::with('station_from', 'station_to', 'icons', 'activity_lines', 'meal_lines', 'partner', 'station_lines', 'routeAddons')
             ->whereIn('id', $routeIds)
             ->where('station_from_id', $stationFromId)
-            ->where('status','CO')
-            ->where('isactive','Y');
-        if($stationToId !='all' && $stationToId !=''){
+            ->where('status', 'CO')
+            ->where('isactive', 'Y');
+        if ($stationToId != 'all' && $stationToId != '') {
             $routes = $routes->where('station_to_id', $stationToId);
         }
 
@@ -51,7 +52,7 @@ class RouteHelper
             ->join('stations', 'routes.station_from_id', '=', 'stations.id')
             ->where('routes.isactive', 'Y')
             ->groupBy('stations.id', 'stations.name', 'stations.piername', 'stations.nickname')
-            ->orderBy('stations.name','ASC')
+            ->orderBy('stations.name', 'ASC')
             ->Get();
 
         return $stations;
@@ -65,31 +66,31 @@ class RouteHelper
             ->join('stations', 'routes.station_to_id', '=', 'stations.id')
             ->where('routes.isactive', 'Y');
         if ($stationFromId == null || $stationFromId == '' || $stationFromId == 'all') {
-
         } else {
             $stations = $stations->where('routes.station_from_id', $stationFromId);
         }
 
         $stations = $stations->groupBy('stations.id', 'stations.name', 'stations.piername', 'stations.nickname')
-        ->orderBy('stations.name','ASC')->Get();
+            ->orderBy('stations.name', 'ASC')->Get();
 
         return $stations;
     }
 
-    public static function getSectionStationFrom($routeFillter = false){
+    public static function getSectionStationFrom($routeFillter = false)
+    {
         $sections = [];
 
-        if($routeFillter){
+        if ($routeFillter) {
             $sections = Section::with(['stations' => function ($query) {
                 $query->has('stationFrom');
             }])->orderBy('sort', 'ASC')->get();
-        }else{
+        } else {
             $sections = Section::with('stations')->orderBy('sort', 'ASC')->get();
         }
 
         $_section = $sections;
-        foreach($_section as $index => $section){
-            if(sizeof($section->stations) ==0 || is_null($section->stations)){
+        foreach ($_section as $index => $section) {
+            if (sizeof($section->stations) == 0 || is_null($section->stations)) {
                 unset($sections[$index]);
             }
         }
@@ -97,29 +98,29 @@ class RouteHelper
         return $sections;
     }
 
-    public static function getSectionStationTo($routeFillter = false,$stationFromId = NULL){
+    public static function getSectionStationTo($routeFillter = false, $stationFromId = NULL)
+    {
         $sections = [];
 
-        if($routeFillter){
-            if($stationFromId !='' && $stationFromId !=null){
-                $routes = Route::select('station_to_id as id')->where('station_from_id',$stationFromId)->get();
+        if ($routeFillter) {
+            if ($stationFromId != '' && $stationFromId != null) {
+                $routes = Route::select('station_to_id as id')->where('station_from_id', $stationFromId)->get();
 
-                $sections = Section::with(['stations' => function ($query) use($routes) {
-                    $query->whereIn('id',$routes);
+                $sections = Section::with(['stations' => function ($query) use ($routes) {
+                    $query->whereIn('id', $routes);
                 }])->orderBy('sort', 'ASC')->get();
-            }else{
+            } else {
                 $sections = Section::with(['stations' => function ($query) {
                     $query->has('stationTo');
                 }])->orderBy('sort', 'ASC')->get();
             }
-
-        }else{
+        } else {
             $sections = Section::with('stations')->orderBy('sort', 'ASC')->get();
         }
 
         $_section = $sections;
-        foreach($_section as $index => $section){
-            if(sizeof($section->stations) ==0 || is_null($section->stations)){
+        foreach ($_section as $index => $section) {
+            if (sizeof($section->stations) == 0 || is_null($section->stations)) {
                 unset($sections[$index]);
             }
         }
@@ -130,16 +131,16 @@ class RouteHelper
     public static function getRoutes($stationFromId, $stationToId)
     {
         $routes = Route::where('isactive', 'Y')
-            ->with(['station_from', 'station_to','partner.image', 'icons']);
-        if(!is_null($stationFromId) && $stationFromId !='' && $stationFromId !='all'){
+            ->with(['station_from', 'station_to', 'partner.image', 'icons']);
+        if (!is_null($stationFromId) && $stationFromId != '' && $stationFromId != 'all') {
             $routes = $routes->where('station_from_id', $stationFromId);
         }
 
-        if(!is_null($stationToId) && $stationToId !='' && $stationToId !='all'){
+        if (!is_null($stationToId) && $stationToId != '' && $stationToId != 'all') {
             $routes = $routes->where('station_to_id', $stationToId);
         }
 
-        $routes  = $routes->orderBy('station_from_id', 'ASC')->orderBy('station_to_id','ASC')
+        $routes  = $routes->orderBy('station_from_id', 'ASC')->orderBy('station_to_id', 'ASC')
             ->orderBy('depart_time', 'ASC')
             ->get();
         //dd($routes);
--- resources/views/pages/route_schedules/show.blade.php
+++ resources/views/pages/route_schedules/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.default')
 @section('page-title')
-    <h2 class="ms-2 mb-0" id="station-page-title"><span class="text-main-color-2">{{ $route->station_from->name }} <i
-                class="fa-solid fa-angles-right px-2"></i>
-            {{ $route->station_to->name }}</span> Calendar view</h2>
+<h2 class="ms-2 mb-0" id="station-page-title"><span class="text-main-color-2">{{ $route->station_from->name }} <i class="fa-solid fa-angles-right px-2"></i>
+        {{ $route->station_to->name }}</span> {{ date('H:i', strtotime($route->depart_time))}}/{{ date('H:i', strtotime($route->arrive_time))}}</h2>
 
 @stop
 
 @section('content')
+
 <form novalidate class="bs-validate" id="frm-fillter" method="GET" action="{{ route('routeSchedules.show', ['routeSchedule' => $routeId]) }}">
     <div class="row">
         <div class="col-12 col-lg-4">
@@ -16,7 +16,7 @@
                     <div class="form-floating mb-3">
                         <select class="form-select" id="start_year" name="start_year">
                             @foreach ($years as $year)
-                                <option value="{{$year->y}}" @selected($startYear==((int)$year->y))>{{$year->y}}</option>
+                            <option value="{{$year->y}}" @selected($startYear==((int)$year->y))>{{$year->y}}</option>
                             @endforeach
                         </select>
                         <label for="start_year">Start Year</label>
@@ -26,7 +26,7 @@
                     <div class="form-floating mb-3">
                         <select class="form-select" id="end_year" name="end_year">
                             @foreach ($years as $year)
-                                <option value="{{$year->y}}" @selected($endYear==((int)$year->y))>{{$year->y}}</option>
+                            <option value="{{$year->y}}" @selected($endYear==((int)$year->y))>{{$year->y}}</option>
                             @endforeach
                         </select>
                         <label for="end_year">End Year</label>
@@ -37,55 +37,55 @@
 
     </div>
 </form>
-    <hr>
-    <div class="row">
-        @foreach ($yearCalendar as $monthCalendar)
-            <div class="col-12 col-lg-3 p-2 ">
-                <h5 class="text-center">{{ $monthCalendar['name'] }}</h5>
-                <table class="table table-bordered table-align-middle">
-                    <thead class="">
-                        <tr class="text-end">
-                            <th>SUN</th>
-                            <th>MON</th>
-                            <th>TUE</th>
-                            <th>WED</th>
-                            <th>THU</th>
-                            <th>FRI</th>
-                            <th>SAT</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($monthCalendar['calendar'] as $row)
-                            <tr>
-                                @foreach ($row as $col)
-                                    <td
-                                        class="text-end @if (isset($routeDailyMaps[$col['date']]) && $routeDailyMaps[$col['date']] == 'Y') text-success
+<hr>
+<div class="row">
+    @foreach ($yearCalendar as $monthCalendar)
+    <div class="col-12 col-lg-3 p-2 ">
+        <h5 class="text-center">{{ $monthCalendar['name'] }}</h5>
+        <table class="table table-bordered table-align-middle">
+            <thead class="">
+                <tr class="text-end">
+                    <th>SUN</th>
+                    <th>MON</th>
+                    <th>TUE</th>
+                    <th>WED</th>
+                    <th>THU</th>
+                    <th>FRI</th>
+                    <th>SAT</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($monthCalendar['calendar'] as $row)
+                <tr>
+                    @foreach ($row as $col)
+                    <td class="text-end @if (isset($routeDailyMaps[$col['date']]) && $routeDailyMaps[$col['date']] == 'Y') text-success
                                         @else text-danger @endif">
-                                        <strong class="@if ($col['current_month'] == 'N') text-gray-300 @endif">
-                                            {{ $col['day'] }}</strong>
+                        <strong class="@if ($col['current_month'] == 'N') text-gray-300 @endif">
+                            {{ $col['day'] }}</strong>
 
-                                    </td>
-                                @endforeach
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        @endforeach
+                    </td>
+                    @endforeach
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+    @endforeach
+</div>
 @stop
 
 @section('script')
-    <script>
-        $(document).ready(function() {
-            $('#start_year, #end_year').change(function() {
-                $('#page-loader').show();
-                //const selectedMonth = $('#month').val();
-                //const selectedYear = $('#year').val();
+<script>
+    $(document).ready(function() {
+        $('#start_year, #end_year').change(function() {
+            $('#page-loader').show();
+            //const selectedMonth = $('#month').val();
+            //const selectedYear = $('#year').val();
 
-                $('#frm-fillter').submit();
-               // console.log(`Selected Month: ${selectedMonth}, Selected Year: ${selectedYear}`);
-            });
+            $('#frm-fillter').submit();
+            // console.log(`Selected Month: ${selectedMonth}, Selected Year: ${selectedYear}`);
         });
-    </script>
+    });
+
+</script>
 @stop
